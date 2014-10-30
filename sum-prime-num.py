23795ea866d3c2cc047e749f499e5d641ec055ef
old-willow <robert.kolozsi@gmail.com>
2014-10-30 23:52:38 +0100

    Fixed bug for the first pass/combination. It finds 3 solutions if sequence allways starts with first element.

--- sum-prime-num.py
+++ sum-prime-num.py
@@ -11,21 +11,53 @@ new_list = []
 
 result_list = []
 result_collection = []
-remain = []
+#remain = []
 first = None
+
+def find_all():
+    global result_list
+    global result_collection
+
+    #if not result_collection:
+    #    one_pass(l)
+    #else:
+    #    one_pass(l)
+    one_pass(l)
+    one_pass(l)
+    one_pass(l)
+
 
 def one_pass(the_list):
     """Recursive function."""
     global first
+    global result_list
+    global result_collection
+    remain = []
+
+    #already_checked = False
+
     tmp_remain = the_list
+
     if not first:
-        first = l[0]
-    else:
-        first = the_list[-1]
+        first = l[0]  # If running for the first time per combination.
+    #else:
+        # The last element is used if we use recursion.
+        #first = the_list[-1]
 
     for j in the_list:
         if j not in result_list and first != j:
             second = j
+            #print("first: " + str(first) + " ; second: " + str(second))
+
+            # This whole condition is ignored for the first pass.
+            if first == the_list[0] and result_collection and check_pair(first, second):
+                #print("already have collection")
+                #already_checked = True
+                remain.append(second)
+                continue
+
+            # this is for first pass
+            #print("no collection yet")
             result = first + second
             if is_prime(result):
                 if first not in result_list:
@@ -35,11 +67,27 @@ def one_pass(the_list):
             else:
                 remain.append(second)
 
+    # This is out of loop.
+    # If we didn't finished the pass start recursion.
     if remain and remain != tmp_remain:
+        #print("recursion")
         one_pass(remain)
     else:
         result_collection.append(result_list)
-        print result_collection
+        first = None
+        second = None  # Not sure if this has any affect?
+        result_list = []
+        remain = []
+        #already_checked = False
+        #print result_collection
+
+def check_pair(first, second):
+    #print("first: " + str(first) + " ; second: " + str(second))
+    for element in result_collection:
+        #if element[:2] == [first, second]:
+        if element[0] == first and element[1] == second:
+            return True
+    return False
 
 
 def print_is_prime():
@@ -52,7 +100,7 @@ def print_is_prime():
 def main():
     #print_is_prime()
     #do_the_math()
-    one_pass(l)
+    find_all()
 
 
 def is_prime(number):
@@ -75,3 +123,4 @@ def is_prime(number):
 
 if __name__ == '__main__':
     main()
+    print result_collection
